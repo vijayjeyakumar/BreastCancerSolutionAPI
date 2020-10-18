281d7760fa7dad26b0a3459e48699368eb6afcdd
vijay Jeyakumar <30817201+vijayjeyakumar@users.noreply.github.com>
2020-10-18 15:58:39 +0530

    Add files via upload

---
+++ RiskPrediction.py
@@ -0,0 +1,26 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('PredictionDataset.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [6])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+Risk Prediction Test:
+y_pred = regressor.predict(X_test)
+
+
+
